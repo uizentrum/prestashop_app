faf569023736233a23edfb2b301d3c1a7b51c0f2
uizentrum <uizentrum@users.noreply.github.com>
2020-10-27 16:38:30 +0100

    27_10_2020

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,34 +1,98 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Container(
-          margin: EdgeInsets.all(0.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.network('http://pres.india1-bernau.de/img/logo.jpg',
-                    width: 250.0,
-                    height: 80.0,
-                  ),
-                ],
-              ),
-            ],
+    return Scaffold(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.network(
+                  'http://pres.india1-bernau.de/img/logo.jpg',
+                  width: 250.0,
+                  height: 80.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FlatButton(
+                      color: Colors.grey,
+                      disabledColor: Colors.grey,
+                      height: 50.0,
+                      minWidth: 120.0,
+                      onPressed: () {
+                        /*Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) => ));*/
+                      },
+                      child: Text(
+                        'Women',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    FlatButton(
+                      color: Colors.grey,
+                      disabledColor: Colors.grey,
+                      height: 50.0,
+                      minWidth: 120.0,
+                      onPressed: () {
+                        /*Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) => ));*/
+                      },
+                      child: Text(
+                        'Dresses',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    FlatButton(
+                      color: Colors.grey,
+                      disabledColor: Colors.grey,
+                      height: 50.0,
+                      minWidth: 120.0,
+                      onPressed: () {
+                        /*Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) => ));*/
+                      },
+                      child: Text(
+                        'T-Shirts',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 6.0,
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
